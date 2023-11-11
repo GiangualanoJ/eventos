622426c6b2ce5a1b782e4119151806629d0b44c9
Josefina <giangualanoj@gmail.com>
2023-11-11 11:55:40 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'primereact/resources/themes/lara-light-teal/theme.css'; 
+import 'primereact/resources/primereact.css';                     
+import 'primeicons/primeicons.css';                            
+import 'primeflex/primeflex.css';                                 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
